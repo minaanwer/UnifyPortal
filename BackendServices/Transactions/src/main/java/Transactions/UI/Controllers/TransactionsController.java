faa60c96ff7abbfc9668f1724d99a738e1e8ab37
mina anwer <minaanwer05@gmail.com>
2020-06-06 18:55:18 +0200

    update

--- BackendServices/Transactions/src/main/java/Transactions/UI/Controllers/TransactionsController.java
+++ BackendServices/Transactions/src/main/java/Transactions/UI/Controllers/TransactionsController.java
@@ -1,8 +1,12 @@
 package Transactions.UI.Controllers;
 
+import Transactions.Shared.Exceptions.ResourceNotFoundException;
+import Transactions.UI.Models.TransactionModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,8 +28,12 @@ public class TransactionsController {
     }
 
 
+    @GetMapping("/{id}")
+   public ResponseEntity<TransactionModel> GetTransactionById(@PathVariable long id)  throws ResourceNotFoundException {
 
-
-
+       // Employee employee = employeeRepository.findById(id)
+        //        .orElseThrow(()-> new ResourceNotFoundException("Employee is not found id : "+id));
+        return ResponseEntity.ok().body(new TransactionModel("205050"));
+    }
 
 }
---
+++ BackendServices/Transactions/src/test/java/Transactions/TransactionControllerTests.java
@@ -0,0 +1,56 @@
+package Transactions;
+
+import Domain.AbstractTest;
+import Transactions.UI.Models.TransactionModel;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.Assert.assertEquals;
+
+public class TransactionControllerTests extends AbstractTest {
+
+
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Test
+    public void GetTransactionStatus_shouldReturn_ok() throws Exception {
+     String uri = "/api/transaction/status" ;
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+
+        assertEquals(200,status);
+         String content = mvcResult.getResponse().getContentAsString();
+         assertEquals(true,content.equals("transaction controller says: i'm ok!") );
+
+    }
+
+
+    @Test
+    public void GetTransactionById_ShouldReturn_Transaction_with_Correct_Id() throws Exception {
+        String uri = "/api/transaction/" ;
+        String transactionId = "11";
+        uri+=transactionId;
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+
+        assertEquals(200,status);
+        String content = mvcResult.getResponse().getContentAsString();
+
+        TransactionModel trx = super.mapFromJson(content,TransactionModel.class);
+        
+    }
+
+
+}
