74d28f801bbc0a0c90891139d0fb9891e0816674
mina anwer <minaanwer05@gmail.com>
2020-06-08 00:15:09 +0200

    update

--- BackendServices/Transactions/src/main/java/Transactions/Shared/Exceptions/GlobalExceptionHandler.java
+++ BackendServices/Transactions/src/main/java/Transactions/Shared/Exceptions/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
-package Transactions.Shared.Exceptions;
+package Transactions.Shared.ExceptionHandlers;
 
+import Transactions.Shared.Exceptions.ErrorDetails;
+import Transactions.Shared.Exceptions.ResourceNotFoundException;
 import Transactions.UI.Models.ResponseModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.Date;
 
@@ -20,6 +24,16 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(responseModel, HttpStatus.NOT_FOUND);
     }
 
+
+        @ExceptionHandler(NoHandlerFoundException.class)
+        @ResponseStatus(HttpStatus.NOT_FOUND)
+        public ResponseEntity<ResponseModel> handleNoHandlerFound(NoHandlerFoundException ex, WebRequest request) {
+            ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+            ResponseModel<ErrorDetails> ResponseModel = new ResponseModel<>("001","any custom error msg", errorDetails);
+            return new ResponseEntity<>(ResponseModel,HttpStatus.NOT_FOUND);
+        }
+
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
       //todo : remove exception message , return General Issue to the Customer , we keep it for debug purposes.
@@ -27,4 +41,5 @@ public class GlobalExceptionHandler {
         ResponseModel<ErrorDetails> responseModel = new ResponseModel<>("999","any custom error msg", errorDetails);
         return new ResponseEntity<>(responseModel, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
 }
