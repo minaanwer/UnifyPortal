9e77e32a162ecc67193b5131d53e6ccb2ab806fe
mina anwer <minaanwer05@gmail.com>
2020-06-07 23:47:20 +0200

    update

--- BackendServices/Transactions/src/main/java/Transactions/Shared/Exceptions/GlobalExceptionHandler.java
+++ BackendServices/Transactions/src/main/java/Transactions/Shared/Exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package Transactions.Shared.Exceptions;
 
+import Transactions.UI.Models.ResponseModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,17 +15,16 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+        ResponseModel<ErrorDetails> responseModel = new ResponseModel<>("001","any custom error msg", errorDetails);
+        return new ResponseEntity<>(responseModel, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
       //todo : remove exception message , return General Issue to the Customer , we keep it for debug purposes.
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+        ResponseModel<ErrorDetails> responseModel = new ResponseModel<>("999","any custom error msg", errorDetails);
+        return new ResponseEntity<>(responseModel, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-
 }
