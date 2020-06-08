e0cc3bee6af0b92e6b560fa6385a540ed2308257
mina anwer <minaanwer05@gmail.com>
2020-06-08 22:35:09 +0200

    update

--- UnifyPortal/src/app/components/home/home.component.ts
+++ UnifyPortal/src/app/components/home/home.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    $(document).ready(function () {
+      //alert('I am Called From jQuery');
+      $.HSCore.components.HSGoTo.init('.js-go-to');
+      $.HSCore.components.HSHeader.init($('#js-header'));
+      $.HSCore.helpers.HSHamburgers.init('.hamburger');
+
+      $('.js-mega-menu').HSMegaMenu({
+        event: 'hover',
+        pageContainer: $('.container'),
+        breakpoint: 991
+      });
+
+    });
+
   }
 
 }
