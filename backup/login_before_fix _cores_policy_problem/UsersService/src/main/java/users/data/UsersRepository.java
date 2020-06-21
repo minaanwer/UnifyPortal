90b4ab8a32b32be3c56765f56e9826b15d66857a
mina anwer <minaanwer05@gmail.com>
2020-06-21 11:46:12 +0200

    update

--- backup/login_before_fix _cores_policy_problem/UsersService/src/main/java/users/data/UsersRepository.java
+++
@@ -1,6 +0,0 @@
-package users.data;
-import org.springframework.data.repository.CrudRepository;
-public interface UsersRepository extends CrudRepository<userEntity,Long> {
- userEntity findByEmail(String s);
- userEntity findByuserId(String userId);
-}
